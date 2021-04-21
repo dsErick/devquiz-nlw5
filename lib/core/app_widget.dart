19368d25169fc90d1891677bc8c9fc89b5e92062
Erick Silva <ericksilvablue@gmail.com>
2021-04-20 21:51:46 -0300

    Splash page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:dev_quiz/splash/splash_page.dart';
+
 class AppWidget extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
             title: 'DevQuiz',
             debugShowCheckedModeBanner: false,
-            home: Scaffold(
-            ),
+            home: SplashPage(),
         );
     }
 }
