0ef62928ff0f71928defe12b94403215e9d301b8
Erick Silva <ericksilvablue@gmail.com>
2021-05-07 14:51:12 -0300

    Navegando da SplashPage para a HomePage

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'package:dev_quiz/home/home_page.dart';
 import 'package:dev_quiz/splash/splash_page.dart';
-import 'package:dev_quiz/challenge/challenge_page.dart';
 
 class AppWidget extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'DevQuiz',
-            debugShowCheckedModeBanner: false,
-            home: HomePage(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DevQuiz',
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
 }
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -2,26 +2,31 @@ import 'package:flutter/material.dart';
 
 import 'package:dev_quiz/core/app_gradients.dart';
 import 'package:dev_quiz/core/app_images.dart';
+import 'package:dev_quiz/home/home_page.dart';
 
 class SplashPage extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        // print(MediaQuery.of(context));
-        // print(Theme.of(context).textTheme);
-        
-        return Scaffold(
-            body: Container(
-                decoration: BoxDecoration(
-                    gradient: AppGradients.linear,
-                ),
-                child: Center(
-                    child: Image.asset(
-                        AppImages.logo,
-                        height: MediaQuery.of(context).size.height * .6,
-                        fit: BoxFit.fitHeight,
-                    ),
-                )
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2))
+      .then((_) {
+        Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (_) => HomePage(),
+        ));
+      });
+    
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(
+            AppImages.logo,
+            height: MediaQuery.of(context).size.height * .6,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+    );
+  }
 }
